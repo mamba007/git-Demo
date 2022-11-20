1425a404028f9ca497605c83b01d6aa1d6ce7247
mamba007 <ga1998urav@gmail.com>
2022-11-20 06:49:56 +0530

    Added If Condition to check the data

--- src/main/java/com/Gaurav/Git/Git_Demo/App.java
+++ src/main/java/com/Gaurav/Git/Git_Demo/App.java
@@ -3,6 +3,9 @@ package com.Gaurav.Git.Git_Demo;
 public class App {
 	public void addData(String data) {
 		System.out.println("In a addData Method" + data);
+		if(data.equals("Gaurav")) {
+			System.out.println("Welcome");
+		}
 	}
 
 	public static void main(String[] args) {
