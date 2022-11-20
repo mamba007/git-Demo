0b05652250d78d734bac943847bd877fd2995903
mamba007 <ga1998urav@gmail.com>
2022-11-20 06:47:09 +0530

    Added Git Demo Project

---
+++ src/main/java/com/Gaurav/Git/Git_Demo/App.java
@@ -0,0 +1,11 @@
+package com.Gaurav.Git.Git_Demo;
+
+public class App {
+	public void addData(String data) {
+		System.out.println("In a addData Method" + data);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
